a43c51d669040ebb43cb08f45311764c347cb4a4
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-12-28 14:22:20 +0700

    frist commit

---
+++ InstagramClone/Controllers/Core tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Macbook on 28/12/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
